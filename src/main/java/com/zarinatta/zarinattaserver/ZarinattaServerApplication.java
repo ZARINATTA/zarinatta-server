12bc979934861b6374a30090efe9e23c8d5c4a01
JunHyuk Yu <ericyoo0107@gmail.com>
2024-09-01 15:28:32 +0900

    first commit

---
+++ src/main/java/com/zarinatta/zarinattaserver/ZarinattaServerApplication.java
@@ -0,0 +1,13 @@
+package com.zarinatta.zarinattaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZarinattaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZarinattaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zarinatta/zarinattaserver/ZarinattaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zarinatta.zarinattaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZarinattaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
