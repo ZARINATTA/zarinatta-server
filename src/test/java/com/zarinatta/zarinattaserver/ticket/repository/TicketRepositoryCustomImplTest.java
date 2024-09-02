4ef279ca356773064a219c18da7bb22a950d2d73
seokyung <sk000801@naver.com>
2024-09-02 21:44:48 +0900

    chore : 배포 자동화 코드 추가 및 test 코드 @SpringBootTest 잠시 주석 처리

--- src/test/java/com/zarinatta/zarinattaserver/ticket/repository/TicketRepositoryCustomImplTest.java
+++ src/test/java/com/zarinatta/zarinattaserver/ticket/repository/TicketRepositoryCustomImplTest.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+//@SpringBootTest
 @Profile("test")
 class TicketRepositoryCustomImplTest {
 
--- src/test/java/com/zarinatta/zarinattaserver/ticket/service/TicketServiceTest.java
+++ src/test/java/com/zarinatta/zarinattaserver/ticket/service/TicketServiceTest.java
@@ -19,7 +19,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+//@SpringBootTest
 @Profile("test")
 class TicketServiceTest {
 
