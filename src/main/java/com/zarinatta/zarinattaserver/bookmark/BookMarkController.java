e90c601612d8e0c72f61665c429b5889d9d408b4
JunHyuk Yu <ericyoo0107@gmail.com>
2024-09-20 14:32:20 +0900

    feat: 즐겨찾기 삭제 API 개발

--- src/main/java/com/zarinatta/zarinattaserver/bookmark/BookMarkController.java
+++ src/main/java/com/zarinatta/zarinattaserver/bookmark/BookMarkController.java
@@ -4,10 +4,11 @@ import com.zarinatta.zarinattaserver.auth.service.JwtService;
 import com.zarinatta.zarinattaserver.bookmark.dto.BookMarkCreateRequest;
 import com.zarinatta.zarinattaserver.entity.BookMark;
 import com.zarinatta.zarinattaserver.entity.User;
-import com.zarinatta.zarinattaserver.exception.exception.UserNotFoundException;
+import com.zarinatta.zarinattaserver.exception.exception.NotFound.UserNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,9 +27,10 @@ public class BookMarkController {
     private final JwtService jwtService;
 
     @GetMapping("/search")
+    @ResponseStatus(HttpStatus.OK)
     public List<BookMarkSearchResponse> searchBookMark(@RequestHeader(value = HEADER_AUTHORIZATION) String authorizationHeader, @RequestParam("ticketIds") List<Long> ticketIds) {
         User user = jwtService.findUserByToken(authorizationHeader)
-                .orElseThrow(() -> new UserNotFoundException("메서드 : searchBookMark"));
+                .orElseThrow(() -> new UserNotFoundException("searchBookMark"));
         List<BookMark> bookMarks = bookMarkRepository.findAllByTicketIdInAndUserId(ticketIds, user);
         List<BookMarkSearchResponse> response = bookMarks.stream()
                 .map(bookMark -> BookMarkSearchResponse.of(bookMark.getTicket().getId(), true))
@@ -37,10 +39,20 @@ public class BookMarkController {
     }
 
     @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
     public String createBookMark(@RequestHeader(value = HEADER_AUTHORIZATION) String authorizationHeader, @RequestBody @Valid BookMarkCreateRequest bookMarkCreateRequest) {
         User user = jwtService.findUserByToken(authorizationHeader)
-                .orElseThrow(() -> new UserNotFoundException("메서드 : createBookMark"));
+                .orElseThrow(() -> new UserNotFoundException("createBookMark"));
         bookMarkService.createBookMark(user, bookMarkCreateRequest);
         return "TicketId: " + bookMarkCreateRequest.getTicketId() + " - 즐겨찾기 추가 완료";
+    }
+
+    @DeleteMapping("/delete/{bookMarkId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public String deleteBookMark(@RequestHeader(value = HEADER_AUTHORIZATION) String authorizationHeader, @PathVariable Long bookMarkId) {
+        User user = jwtService.findUserByToken(authorizationHeader)
+                .orElseThrow(() -> new UserNotFoundException("deleteBookMark"));
+        bookMarkService.deleteBookMark(user, bookMarkId);
+        return "BookMarkId: " + bookMarkId + " - 즐겨찾기 삭제 완료";
     }
 }
--- src/main/java/com/zarinatta/zarinattaserver/bookmark/BookMarkService.java
+++ src/main/java/com/zarinatta/zarinattaserver/bookmark/BookMarkService.java
@@ -4,7 +4,9 @@ import com.zarinatta.zarinattaserver.bookmark.dto.BookMarkCreateRequest;
 import com.zarinatta.zarinattaserver.entity.BookMark;
 import com.zarinatta.zarinattaserver.entity.Ticket;
 import com.zarinatta.zarinattaserver.entity.User;
-import com.zarinatta.zarinattaserver.exception.exception.BookMarkNotFoundException;
+import com.zarinatta.zarinattaserver.exception.exception.NotFound.BookMarkNotFoundException;
+import com.zarinatta.zarinattaserver.exception.exception.NotFound.TicketNotFoundException;
+import com.zarinatta.zarinattaserver.exception.exception.NotPermit.BookMarkPermitException;
 import com.zarinatta.zarinattaserver.ticket.repository.TicketRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,8 +25,18 @@ public class BookMarkService {
     @Transactional
     public void createBookMark(User user, BookMarkCreateRequest bookMarkCreateRequest) {
         Ticket ticket = ticketRepository.findById(bookMarkCreateRequest.getTicketId())
-                .orElseThrow(() -> new BookMarkNotFoundException("메서드 : createBookMark"));
+                .orElseThrow(() -> new TicketNotFoundException("createBookMark"));
         BookMark bookMark = BookMark.from(bookMarkCreateRequest, ticket, user);
         bookMarkRepository.save(bookMark);
+    }
+
+    @Transactional
+    public void deleteBookMark(User user, Long bookMarkId) {
+        BookMark bookMark = bookMarkRepository.findById(bookMarkId)
+                .orElseThrow(() -> new BookMarkNotFoundException("deleteBookMark"));
+        if (!bookMark.getUser().getId().equals(user.getId())) {
+            throw new BookMarkPermitException("deleteBookMark", "userID : " + user.getId() + " - bookMarkID : " + bookMark.getId() + " 삭제 불가");
+        }
+        bookMarkRepository.delete(bookMark);
     }
 }
