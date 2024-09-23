589aeac84d01fd2103411635135c6d837f98b389
seokyung <sk000801@naver.com>
2024-09-23 22:24:23 +0900

    feat : 마스터 토큰 발급하는 API 구현 (사용에 주의!)
    (cherry picked from commit 336f95620557508a79f229dd730ed0ec3dadfae3)

--- src/main/java/com/zarinatta/zarinattaserver/auth/controller/AuthController.java
+++ src/main/java/com/zarinatta/zarinattaserver/auth/controller/AuthController.java
@@ -1,9 +1,11 @@
 package com.zarinatta.zarinattaserver.auth.controller;
 
+import com.zarinatta.zarinattaserver.auth.dto.MasterTokenDto;
 import com.zarinatta.zarinattaserver.auth.dto.RedirectDto;
 import com.zarinatta.zarinattaserver.auth.dto.TestDto;
 import com.zarinatta.zarinattaserver.auth.dto.TokenResponseDto;
 import com.zarinatta.zarinattaserver.auth.service.AuthService;
+import com.zarinatta.zarinattaserver.auth.service.JwtService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +22,7 @@ import java.util.Map;
 @Slf4j
 public class AuthController {
     private final AuthService authService;
+    private final JwtService jwtService;
 
     @GetMapping("/redirect")
     public ResponseEntity<RedirectDto> redirect() throws IOException, InterruptedException {
@@ -81,6 +84,24 @@ public class AuthController {
 
         return ResponseEntity.status(HttpStatus.OK)
                 .header("Set-Cookie", deletedCookie.toString()).build();
+    }
+
+    @GetMapping("/master")
+    public ResponseEntity<Map<String, String>> generateMasterToken(HttpServletResponse response) {
+
+        MasterTokenDto masterTokenDto = jwtService.createMasterToken();
+
+        ResponseCookie cookie = ResponseCookie.from("skt", masterTokenDto.getAccessToken())
+                .maxAge(-1) // 쿠키 만료 시간 설정
+                .path("/") // 쿠키의 경로 설정
+                .httpOnly(true) // 보안 설정
+                .build();
+
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .header("Set-Cookie", masterTokenDto.getAccessToken())
+                .body(Map.of("refreshToken", masterTokenDto.getRefreshToken()));
     }
 
     @GetMapping("/test")
