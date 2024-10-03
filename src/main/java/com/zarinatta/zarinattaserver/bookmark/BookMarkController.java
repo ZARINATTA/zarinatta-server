85e840b18b61ee6092a20855e42b6ee3a007e278
JunHyuk Yu <ericyoo0107@gmail.com>
2024-10-03 15:31:33 +0900

    feat: 즐겨찾기 목록 조회 API 반환 값 변경

--- src/main/java/com/zarinatta/zarinattaserver/bookmark/BookMarkController.java
+++ src/main/java/com/zarinatta/zarinattaserver/bookmark/BookMarkController.java
@@ -1,15 +1,14 @@
 package com.zarinatta.zarinattaserver.bookmark;
 
 import com.zarinatta.zarinattaserver.auth.service.JwtService;
+import com.zarinatta.zarinattaserver.entity.BookMark;
+import com.zarinatta.zarinattaserver.entity.User;
 import com.zarinatta.zarinattaserver.exception.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import com.zarinatta.zarinattaserver.entity.BookMark;
-import com.zarinatta.zarinattaserver.entity.User;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static com.zarinatta.zarinattaserver.enums.Values.HEADER_AUTHORIZATION;
 
@@ -23,13 +22,11 @@ public class BookMarkController {
     private final JwtService jwtService;
 
     @GetMapping("/search")
-    public List<BookMarkSearchResponse> searchBookMark(@RequestHeader(value = HEADER_AUTHORIZATION) String authorizationHeader, @RequestParam("ticketIds") List<Long> ticketIds) {
+    public List<Long> searchBookMark(@RequestHeader(value = HEADER_AUTHORIZATION) String authorizationHeader, @RequestParam("ticketIds") List<Long> ticketIds) {
         User user = jwtService.findUserByToken(authorizationHeader)
                 .orElseThrow(() -> new UserNotFoundException("사용자를 찾을 수 없습니다."));
         List<BookMark> bookMarks = bookMarkRepository.findAllByTicketIdInAndUserId(ticketIds, user);
-        List<BookMarkSearchResponse> response = bookMarks.stream()
-                .map(bookMark -> BookMarkSearchResponse.of(bookMark.getTicket().getId(), true))
-                .collect(Collectors.toList());
+        List<Long> response = bookMarks.stream().map(bookMark -> bookMark.getId()).toList();
         return response;
     }
 }
