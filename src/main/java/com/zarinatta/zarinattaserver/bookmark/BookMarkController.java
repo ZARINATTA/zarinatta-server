42de39aec2da3296963ee3fda301896346639bf3
JunHyuk Yu <ericyoo0107@gmail.com>
2024-10-03 15:37:12 +0900

    feat: 유저 조회 로직 변경

--- src/main/java/com/zarinatta/zarinattaserver/bookmark/BookMarkController.java
+++ src/main/java/com/zarinatta/zarinattaserver/bookmark/BookMarkController.java
@@ -4,13 +4,15 @@ import com.zarinatta.zarinattaserver.auth.service.JwtService;
 import com.zarinatta.zarinattaserver.entity.BookMark;
 import com.zarinatta.zarinattaserver.entity.User;
 import com.zarinatta.zarinattaserver.exception.exception.UserNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
-import static com.zarinatta.zarinattaserver.enums.Values.HEADER_AUTHORIZATION;
 
 @Slf4j
 @RestController
@@ -22,8 +24,9 @@ public class BookMarkController {
     private final JwtService jwtService;
 
     @GetMapping("/search")
-    public List<Long> searchBookMark(@RequestHeader(value = HEADER_AUTHORIZATION) String authorizationHeader, @RequestParam("ticketIds") List<Long> ticketIds) {
-        User user = jwtService.findUserByToken(authorizationHeader)
+    public List<Long> searchBookMark(HttpServletRequest request, @RequestParam("ticketIds") List<Long> ticketIds) {
+        String accessToken = (String) request.getAttribute("accessToken");
+        User user = jwtService.findUserByToken(accessToken)
                 .orElseThrow(() -> new UserNotFoundException("사용자를 찾을 수 없습니다."));
         List<BookMark> bookMarks = bookMarkRepository.findAllByTicketIdInAndUserId(ticketIds, user);
         List<Long> response = bookMarks.stream().map(bookMark -> bookMark.getId()).toList();
