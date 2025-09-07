a3716d1bcc3b1dca9be6aeab7d49722b32c02807
JunHyuk Yu <ericyoo0107@gmail.com>
2025-09-07 16:28:53 +0900

    refactor: ticket 도메인 DTO 타입 변경

--- src/main/java/com/zarinatta/zarinattaserver/bookmark/dto/request/MyBookMarkRequest.java
+++ src/main/java/com/zarinatta/zarinattaserver/bookmark/dto/request/MyBookMarkRequest.java
@@ -1,14 +1,11 @@
 package com.zarinatta.zarinattaserver.bookmark.dto.request;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-public class MyBookMarkRequest {
-    @NotNull(message = "만료된 즐겨 찾기 요청 여부 입력 필수")
-    private Boolean expire;
-    private int page = 1;
-    private int size = 10;
+public record MyBookMarkRequest(
+        @NotNull(message = "만료된 즐겨 찾기 요청 여부 입력 필수")
+        Boolean expire,
+        int page,
+        int size
+) {
 }
