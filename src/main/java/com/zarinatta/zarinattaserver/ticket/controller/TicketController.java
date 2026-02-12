5399aa60f4fdd3cd2a5e5008e39d11c7f95025bf
JunHyuk Yu <ericyoo0107@gmail.com>
2026-02-12 20:11:38 +0900

    chore: Sentry 트래킹 추가

--- src/main/java/com/zarinatta/zarinattaserver/ticket/controller/TicketController.java
+++ src/main/java/com/zarinatta/zarinattaserver/ticket/controller/TicketController.java
@@ -1,15 +1,14 @@
 package com.zarinatta.zarinattaserver.ticket.controller;
 
+import com.zarinatta.zarinattaserver.ticket.dto.request.TicketSearchRequest;
+import com.zarinatta.zarinattaserver.ticket.dto.response.PageTicketResponse;
+import com.zarinatta.zarinattaserver.ticket.service.TicketService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.zarinatta.zarinattaserver.ticket.dto.request.TicketSearchRequest;
-import com.zarinatta.zarinattaserver.ticket.dto.response.PageTicketResponse;
-import com.zarinatta.zarinattaserver.ticket.service.TicketService;
 
 @Slf4j
 @RestController
