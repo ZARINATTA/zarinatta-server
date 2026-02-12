2e21e466bd4d226ed9afe5f6f2afcf520458dc78
JunHyuk Yu <ericyoo0107@gmail.com>
2026-02-12 20:42:50 +0900

    feat: 테스트 완료 후 삭제

--- src/main/java/com/zarinatta/zarinattaserver/ticket/controller/TicketController.java
+++ src/main/java/com/zarinatta/zarinattaserver/ticket/controller/TicketController.java
@@ -29,8 +29,4 @@ public class TicketController {
     public void insertData() {
         ticketService.insertMockData();
     }
-
-    @GetMapping("/sentry/test")
-    public void test() {
-        throw new RuntimeException("sentry 예외 테스트 - 시간 : " + System.currentTimeMillis());}
 }
